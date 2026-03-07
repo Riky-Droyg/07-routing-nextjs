06987ab85682555376de0d7e6758cc5929490da7
Riky-Droyg <riky.droyg@gmail.com>
2026-03-07 09:48:52 +0200

    main 0.3

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -3,12 +3,12 @@
 import { useState } from "react";
 import css from "./NotesPage.module.css";
 import ReactPaginate from "react-paginate";
-import Modal from "../components/Modal/Modal";
-import SearchBox from "../components/SearchBox/SearchBox";
-import NoteForm from "../components/NoteForm/NoteForm";
+import Modal from "../../components/Modal/Modal";
+import SearchBox from "../../components/SearchBox/SearchBox";
+import NoteForm from "../../components/NoteForm/NoteForm";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { useDebounce } from "use-debounce";
-import NoteList from "../components/NoteList/NoteList";
+import NoteList from "../../components/NoteList/NoteList";
 import { noteService } from "../lib/api";
 
 function App() {
